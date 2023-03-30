b245e1345e777bdf7a1dcdd4f55d70e0b2278d3e
asifntnu <drasifpk@hotmail.com>
2023-03-29 21:26:44 -0400

    test actions

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-print("hello world")
-print("hello Again")
+
+
+
+class Component:
+ def foo(self):
+   x =7
+   print(x)
+
+class Composite:
+ def __init__(self, ob):
+  self.ob=ob
+  self.ob.foo()
+
+#this is test
+m=Composite(Component())
